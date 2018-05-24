4f8090268c0ee41c31da7a9bade496faf1cbdd70
Hao Tian <haotian@HaodeMacBook-Pro.local>
2018-05-24 18:20:24 +0800

    Updata No.21

--- Leetcode/no21.py
+++ Leetcode/no21.py
@@ -15,8 +15,8 @@ class ListNode:
         self.val = x
         self.next = None
 
-
-class Solution:
+# My version, too complicated and not right
+class Old_Solution:
     def mergeTwoLists(self, l1, l2):
         """
         :type l1: ListNode
@@ -46,11 +46,8 @@ class Solution:
                 pointer_result.val = pointer_1
                 pointer_result.next = pointer_2
             else:
-                while(isinstance(pointer_2, ListNode) and pointer_2:
+                while(isinstance(pointer_2, ListNode) and pointer_2):
                     pointer_result.val = pointer_2.val
-                
-            
-
         return result
 
 # create a linked list by using list
@@ -72,12 +69,38 @@ def print_list(l):
         print(l.val)
         print_list(l.next)
 
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+
+        current = head = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next # Move the pointer one more to the back
+        if l1:
+            current.next = l1
+        else:
+            current.next = l2
+        return head.next
+
 l1 = create_list([1,2,4])
 l2 = create_list([1,3,4])
 
-s = Solution
-
-# Testing
-if __name__ == "__main__":
-    print_list(s.mergeTwoLists(s,l1,l2))
-    
+"""
+结论：
+我自己写的Linkedlist不适用于正确的解法。
+LC里面本身的链表结构是什么样的呢？
+"""
